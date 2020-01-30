41045df2a8f126614a910970ea8f83be922d8a5b
Pierce Alworth <pbalworth@gmail.com>
2020-01-29 17:47:51 -0700

    Adding seeds for John's Shelter and Mountian Shelter

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Shelter.create(name: "John's Shelter", address: "1550 East 15th", city: "Denver", state: "Colorado", zip: 80206)
+Shelter.create(name: "Mountain Shelter", address: "1234 West 12th", city: "Denver", state: "Colorado", zip: 80204)
