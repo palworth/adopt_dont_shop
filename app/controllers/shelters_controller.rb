5941fa810cb1b8bf72c549209075e32e10e38171
Pierce Alworth <pbalworth@gmail.com>
2020-01-30 18:01:14 -0700

    Adding shelter model fuctionality

--- app/controllers/shelters_controller.rb
+++ app/controllers/shelters_controller.rb
@@ -2,4 +2,23 @@ class SheltersController < ApplicationController
   def index
     @shelters = Shelter.all
   end
+
+  def show
+    @shelter = Shelter.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+    Shelter.new(shelter_params)
+
+    redirect '/shelters'
+  end
+
+  private
+
+  def shelter_params
+    params.permit(:name, :address, :city, :state, :zip)
+  end
 end
