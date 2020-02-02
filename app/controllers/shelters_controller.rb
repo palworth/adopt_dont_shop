0ce8b2bb9dcab2e1214d2dd689ed76eb34929408
Pierce Alworth <pbalworth@gmail.com>
2020-02-02 12:46:02 -0700

    Adding update and edit methods to shelters controller

--- app/controllers/shelters_controller.rb
+++ app/controllers/shelters_controller.rb
@@ -23,6 +23,25 @@ class SheltersController < ApplicationController
     @shelter = Shelter.find(params[:id])
   end
 
+  def update
+    shelter = Shelter.find(params[:id])
+    shelter.update({
+      name: params[:name],
+      address: params[:address],
+      city: params[:city],
+      state: params[:state],
+      zip: params[:zip]
+      })
+
+    shelter.save
+
+    redirect_to "/shelters/#{shelter.id}"
+  end
+
+  def edit
+    @shelter = Shelter.find(params[:id])
+  end
+
 
   private
 
