c28c6f1a03c3413ac8d6b36d561f69dc1aab7804
Pierce Alworth <pbalworth@gmail.com>
2020-02-02 17:45:26 -0700

    Fixing heroku database issues. Final commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,10 @@
+Pet.destroy_all
+Shelter.destroy_all
 
 shelter_1 = Shelter.create(name: "John's Shelter", address: "1550 East 15th", city: "Denver", state: "Colorado", zip: 80206)
 shelter_2 = Shelter.create(name: "Mountain Shelter", address: "1234 West 12th", city: "Denver", state: "Colorado", zip: 80204)
 
-Pet.create(image: "https://gardenandgun.com/wp-content/uploads/2018/04/cash.jpg",
+pet1 = Pet.create(image: "https://gardenandgun.com/wp-content/uploads/2018/04/cash.jpg",
                   name: "Nala",
                   description: "Black lab",
                   age: "2",
@@ -10,7 +12,7 @@ Pet.create(image: "https://gardenandgun.com/wp-content/uploads/2018/04/cash.jpg"
                   adoptable: "yes",
                   shelter_id: shelter_1.id
                   )
-Pet.create(image: "https://huhinteresting.files.wordpress.com/2009/07/buddy31.jpg",
+pet2 = Pet.create(image: "https://huhinteresting.files.wordpress.com/2009/07/buddy31.jpg",
                   name: "Leo",
                   description: "Big dog",
                   age: "4",
